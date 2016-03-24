13d2eb312119e96ba2d23f7f1b2029fab82bf29d
Elliott Pogue <epogue@gmail.com>
2016-03-24 11:45:37 -0400

    Allow Ecto 2.0

--- mix.exs
+++ mix.exs
@@ -22,7 +22,7 @@ defmodule JaSerializer.Mixfile do
   defp deps do
     [{:inflex, "~> 1.4"},
      {:plug, "~> 1.0"},
-     {:ecto, "~> 1.0"},
+     {:ecto, "~> 1.0 or ~> 2.0"},
      {:poison, "~> 1.4 or ~> 2.0"},
      {:earmark, "~> 0.1", only: :dev},
      {:inch_ex, "~> 0.4", only: :docs},
